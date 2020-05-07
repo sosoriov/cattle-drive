fba47976e6c8fa44a0667ad31d684079c7f65d32
Sebastian Osorio Vélez <sebastian.osoriovelez@delen.be>
2020-05-07 11:20:09 +0200

    remove helm init script

--- initialize-helm.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Configuration Path from RKE Provider
-config_path="$(pwd)/kube_config_cluster.yml"
-
-# Initialize Helm
-helm init --kube-context local --kubeconfig "$config_path" --wait
