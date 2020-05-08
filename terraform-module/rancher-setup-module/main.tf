be768a9c4cd17779ce943bf49549368bebbd69be
Sebastian Osorio Vélez <sebastian.osoriovelez@delen.be>
2020-05-08 10:47:05 +0200

    testing kubeconfig

--- terraform-module/rancher-setup-module/main.tf
+++ terraform-module/rancher-setup-module/main.tf
@@ -3,11 +3,13 @@ provider "kubernetes" {
   version = "1.11.1"
 
   host = var.rancher_k8s.host
-  client_certificate     = var.rancher_k8s.client_certificate
-  client_key             = var.rancher_k8s.client_key
-  cluster_ca_certificate = var.rancher_k8s.cluster_ca_certificate
+  # client_certificate     = var.rancher_k8s.client_certificate
+  # client_key             = var.rancher_k8s.client_key
+  # cluster_ca_certificate = var.rancher_k8s.cluster_ca_certificate
 
-  load_config_file = false
+  config_path = "./kubeconfig.yaml"
+
+  load_config_file = true
 }
 
 # Helm provider
