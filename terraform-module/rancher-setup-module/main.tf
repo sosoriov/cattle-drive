aa89586328b1a0cb37d1aad71a2d63b03d222bc2
Sebastian Osorio Vélez <sebastian.osoriovelez@delen.be>
2020-05-08 11:04:50 +0200

    t

--- terraform-module/rancher-setup-module/main.tf
+++ terraform-module/rancher-setup-module/main.tf
@@ -2,7 +2,7 @@
 provider "kubernetes" {
   version = "1.11.1"
 
-  host = var.rancher_k8s.host
+  # host = var.rancher_k8s.host
   # client_certificate     = var.rancher_k8s.client_certificate
   # client_key             = var.rancher_k8s.client_key
   # cluster_ca_certificate = var.rancher_k8s.cluster_ca_certificate
@@ -17,7 +17,7 @@ provider "helm" {
   version = "1.1.1"
 
   kubernetes {
-    host = var.rancher_k8s.host
+    # host = var.rancher_k8s.host
     # client_certificate     = var.rancher_k8s.client_certificate
     # client_key             = var.rancher_k8s.client_key
     # cluster_ca_certificate = var.rancher_k8s.cluster_ca_certificate
